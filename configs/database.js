2991a2132bdb68e099cc13ca7d47e263c2647d97
João Victor <156962782+jvictordev7@users.noreply.github.com>
2025-04-20 19:32:36 -0300

    Deploy final TiDB

--- configs/database.js
+++ configs/database.js
@@ -2,13 +2,17 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: true // ativa SSL com verificação
+  }
 });
 
 module.exports = pool;
